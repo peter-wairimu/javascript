8a06998de00147c629a39e9eacae53259001dd82
peter-wairimu <peter.wairimu@student.moringaschool.com>
2021-12-18 11:03:45 +0300

    initial commmit

---
+++ index.js
@@ -0,0 +1,16 @@
+let countEl = document.getElementById('count');
+
+let count = 0;
+
+function increase() {
+  count++;
+  countEl.innerHTML = count;
+}
+
+
+function save(){
+  console.log(count);
+
+
+
+}
