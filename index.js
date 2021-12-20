bad477993e2fe465150d7757ce4dc09a181c001c
peter-wairimu <peter.wairimu@student.moringaschool.com>
2021-12-20 17:03:55 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 let countEl = document.getElementById('count');
+let saveEl = document.getElementById('save-el'); 
+
 
 let count = 0;
 
@@ -9,8 +11,25 @@ function increase() {
 
 
 function save(){
-  console.log(count);
-
+  let countStr = count + ' - ';
+  saveEl.innerHTML += countStr;
+  countEl.innerHTML = 0;
+  count = 0
 
 
 }
+
+
+
+
+let welcomeEl = document.getElementById('welcome-el');
+
+let name = "Peter Chege Wairimu";
+let greeting = "Hello, Welcome to our web page";
+
+
+welcomeEl.innerHTML = greeting + " " + name + "";
+
+welcomeEl.innerHTML += "!";
+
+
